55fa541dd8d3e4e5122fbbf78efaf137c226f3c2
Edwin3002 <vargas11742@gmail.com>
2022-06-23 21:04:45 -0500

    quitar etiqueta link

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import Link from 'next/Link'
 
 const Error404 = () => {
   return (
-        <Link href="/">
         <h1>Algo a fallado error 404</h1>
-        </Link>
   )
 }
 
