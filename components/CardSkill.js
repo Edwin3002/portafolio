d9713e42795cc26ee08413d014f82c020f624e51
Edwin3002 <vargas11742@gmail.com>
2022-06-24 16:32:54 -0500

    diseño contacto

--- components/CardSkill.js
+++ components/CardSkill.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CardSkill = ({ data }) => {
     return (
-        <div className='bg-purple-500 my-4 mx-auto p-2 w-full  cardMaxW rounded'>
+        <div className=' my-4 mx-auto p-2 w-full  cardMaxW rounded'>
             <div className='imgCard'>
                 <img src={data.img} alt={data.name} />
             </div>
--- components/Contact.js
+++ components/Contact.js
@@ -1,14 +1,33 @@
 import React from 'react'
+import { Facebook, Github, Linkedin, Mail } from '../icons/icons'
 
 const Contact = () => {
   return (
     <div className='my-16  mx-auto w-full lg:w-3/4'>
       <h2 className='font-bold text-3xl lg:text-4xl'>Contact</h2>
-      <p>
+      <p className='my-6 text-xl lg:text-2xl'>
         Contact me if you want us to work together.
       </p>
-      <a></a>
-
+      <div className='container my-16 flex justify-around'>
+        <div className='card'>
+          <a href='https://www.linkedin.com/in/edwin-vargas-ayala/' target=' _blanck'>
+          <div className='logo'><Linkedin/></div>
+          <div className='logoName'>Linkedin</div>
+          </a>
+        </div>
+        <div className='card'>
+        <a href='https://github.com/Edwin3002' target=' _blanck'>
+          <div className='logo'><Github/></div>
+          <div className='logoName'>Github</div>
+          </a>
+        </div>
+        <div className='card'>
+        <a href='mailto:vargas11742@gmail.com' target=' _blanck'>
+          <div className='logo'><Mail/></div>
+          <div className='logoName'>Mail</div>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
