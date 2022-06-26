127cda595a6c23563c91342441a4866770fe3b18
Edwin3002 <vargas11742@gmail.com>
2022-06-25 23:04:23 -0500

    img y portafolio

--- components/CardPortafolio.js
+++ components/CardPortafolio.js
@@ -6,22 +6,17 @@ const CardPortafolio = ({ project }) => {
     return (
         <div className=' my-4 mx-4 cardMaxWPorta rounded-2xl'>
             <div className='imgCardPorta '>
-                <div class="slider">
+                <div className="slider">
                     <ul>
                         <li>
-                            <img src={project.img[0]} alt={project.name} />
+                            <img className='imgP' src={project.img[0]} alt={project.name} />
                         </li>
                         <li>
-                            <img src={project.img[1]} alt={project.name} />
+                            <img className='imgP' src={project.img[1]} alt={project.name} />
                         </li>
                         <li>
-                            <img src={project.img[2]} alt={project.name} />
+                            <img className='imgP' src={project.img[2]} alt={project.name} />
                         </li>
-                        {/* {project.img.map((i) => (
-                            <li>
-                                <img src={i} alt={project.name} />
-                            </li>
-                        ))} */}
                     </ul>
                 </div>
                 {/* <img className='rounded-xl' src='https://res.cloudinary.com/edwin3002/image/upload/v1656100567/portafolio/Screenshot_2022-06-24_145416_flejy9.png' alt='img' /> */}
--- data/projects.js
+++ data/projects.js
@@ -3,30 +3,50 @@ export const projectsPortafolio = [
         id: 1,
         name: 'Parrilla',
         url: 'https://parrilla.vercel.app',
-        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656202100/portafolio/parrilla-1_s0cwbv.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656202127/portafolio/parrilla-2_hbnrio.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656202117/portafolio/parrilla-3_rlnfwk.png'],
+        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656215561/portafolio/parrilla-1_pkoeph.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656216051/portafolio/parrilla-3_xhdxbj.png'],
         github: 'https://github.com/Edwin3002/Restaurante-Parrillla',
         figma: ['https://www.figma.com/file/ihVA2GvbVv2ayN9MG060Vf/Restaurant?node-id=0%3A1', 'https://www.figma.com/proto/ihVA2GvbVv2ayN9MG060Vf/Restaurant?page-id=0%3A1&node-id=0%3A1&viewport=-443%2C143%2C0.74&scaling=scale-down&starting-point-node-id=3%3A3&show-proto-sidebar=1'],
-        technology: 'React.js, Next.js, Tailwind, Figma.',
+        technology: 'React.js, Firebase, React-BootStrap, Redux, Figma.',
         descrption: ''
     },
     {
         id: 2,
         name: 'Tu Asistente Prof',
         url: 'https://demo-day-projects-asistente-prof.vercel.app',
-        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656202052/portafolio/prof-1_osn559.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656202031/portafolio/prof-2_tgeyam.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656202029/portafolio/prof-3_nhny50.png'],
+        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656215546/portafolio/prof-1_oqc3d9.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656215550/portafolio/prof-2_olcyks.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656215553/portafolio/prof-3_edymzi.png'],
         github: 'https://github.com/academia-geek/demo-day-projects-asistente-prof',
         figma: ['https://www.figma.com/file/okvLSbGpf2CwqCzF0jz9WS/Asistente-prof?node-id=0%3A1', ''],
-        technology: 'React.js, Redux, React-BootStrap,Cloudinary, SweetAlert, EmailJS, JS-Pdf, Figma',
+        technology: 'React.js, Redux, React-BootStrap, Cloudinary, SweetAlert, EmailJS, JS-Pdf, Figma, Firebase',
         descrption: ''
     },
     {
-        id: 2,
-        name: '',
-        url: '',
-        img: '',
-        github: '',
-        figma: ['', ''],
-        technology: ['React.js', '', '', 'Figma'],
+        id: 3,
+        name: 'Portafolio',
+        url: 'https://edwin-dev.vercel.app',
+        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656215552/portafolio/portafolio-1_kvmlnm.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656215535/portafolio/portafolio-2_b70rdk.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656215538/portafolio/portafolio-3_pwudi4.png'],
+        github: 'https://github.com/Edwin3002/portafolio',
+        figma: ['https://www.figma.com/file/Vx51s3MdtYjORzN6ixQ4Kw/Portafolio?node-id=0%3A1', ''],
+        technology: 'React.js, Next.js, Tailwind, Figma.',
         descrption: ''
-    }
+    },
+    // {
+    //     id: 4,
+    //     name: 'Portafolio',
+    //     url: 'https://edwin-dev.vercel.app/',
+    //     img: '',
+    //     github: 'https://github.com/Edwin3002/portafolio',
+    //     figma: ['https://www.figma.com/file/Vx51s3MdtYjORzN6ixQ4Kw/Portafolio?node-id=0%3A1', ''],
+    //     technology: 'React.js, Next.js, Tailwind, Figma.',
+    //     descrption: ''
+    // },
+    // {
+    //     id: 5,
+    //     name: 'Portafolio',
+    //     url: 'https://edwin-dev.vercel.app/',
+    //     img: '',
+    //     github: 'https://github.com/Edwin3002/portafolio',
+    //     figma: ['https://www.figma.com/file/Vx51s3MdtYjORzN6ixQ4Kw/Portafolio?node-id=0%3A1', ''],
+    //     technology: 'React.js, Next.js, Tailwind, Figma.',
+    //     descrption: ''
+    // }
 ]
