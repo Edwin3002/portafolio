12ee989b832f6bd8af0a727e54d236fdd91c6d92
Edwin3002 <vargas11742@gmail.com>
2022-06-26 01:31:24 -0500

    more data en los projects

--- data/projects.js
+++ data/projects.js
@@ -2,8 +2,8 @@ export const projectsPortafolio = [
     {
         id: 1,
         name: 'Parrilla',
-        url: 'https://parrilla.vercel.app',
-        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656215561/portafolio/parrilla-1_pkoeph.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656216051/portafolio/parrilla-3_xhdxbj.png'],
+        url: 'https://parrilla.vercel.app/',
+        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656215561/portafolio/parrilla-1_pkoeph.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656215559/portafolio/parrilla-2_dcggsk.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656216051/portafolio/parrilla-3_xhdxbj.png'],
         github: 'https://github.com/Edwin3002/Restaurante-Parrillla',
         figma: ['https://www.figma.com/file/ihVA2GvbVv2ayN9MG060Vf/Restaurant?node-id=0%3A1', 'https://www.figma.com/proto/ihVA2GvbVv2ayN9MG060Vf/Restaurant?page-id=0%3A1&node-id=0%3A1&viewport=-443%2C143%2C0.74&scaling=scale-down&starting-point-node-id=3%3A3&show-proto-sidebar=1'],
         technology: 'React.js, Firebase, React-BootStrap, Redux, Figma.',
@@ -12,17 +12,17 @@ export const projectsPortafolio = [
     {
         id: 2,
         name: 'Tu Asistente Prof',
-        url: 'https://demo-day-projects-asistente-prof.vercel.app',
+        url: 'https://demo-day-projects-asistente-prof.vercel.app/',
         img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656215546/portafolio/prof-1_oqc3d9.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656215550/portafolio/prof-2_olcyks.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656215553/portafolio/prof-3_edymzi.png'],
         github: 'https://github.com/academia-geek/demo-day-projects-asistente-prof',
         figma: ['https://www.figma.com/file/okvLSbGpf2CwqCzF0jz9WS/Asistente-prof?node-id=0%3A1', ''],
-        technology: 'React.js, Redux, React-BootStrap, Cloudinary, SweetAlert, EmailJS, JS-Pdf, Figma, Firebase',
+        technology: 'React.js, Redux, React-BootStrap, Cloudinary, SweetAlert2, EmailJS, JS-Pdf, Figma, Firebase',
         descrption: ''
     },
     {
         id: 3,
         name: 'Portafolio',
-        url: 'https://edwin-dev.vercel.app',
+        url: 'https://edwin-dev.vercel.app/',
         img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656215552/portafolio/portafolio-1_kvmlnm.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656215535/portafolio/portafolio-2_b70rdk.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656215538/portafolio/portafolio-3_pwudi4.png'],
         github: 'https://github.com/Edwin3002/portafolio',
         figma: ['https://www.figma.com/file/Vx51s3MdtYjORzN6ixQ4Kw/Portafolio?node-id=0%3A1', ''],
@@ -31,22 +31,74 @@ export const projectsPortafolio = [
     },
     // {
     //     id: 4,
-    //     name: 'Portafolio',
-    //     url: 'https://edwin-dev.vercel.app/',
+    //     name: 'Tasks',
+    //     url: 'https://landing-fylo.vercel.app/',
     //     img: '',
-    //     github: 'https://github.com/Edwin3002/portafolio',
-    //     figma: ['https://www.figma.com/file/Vx51s3MdtYjORzN6ixQ4Kw/Portafolio?node-id=0%3A1', ''],
-    //     technology: 'React.js, Next.js, Tailwind, Figma.',
+    //     github: 'https://github.com/Edwin3002/landing-page-Fylo',
+    //     figma: ['', ''],
+    //     technology: 'React.js, FontAwesome, Chakra ui.',
     //     descrption: ''
     // },
+    {
+        id: 5,
+        name: 'Landing Page Fylo',
+        url: 'https://landing-fylo.vercel.app/',
+        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656224267/portafolio/landing-1_z9ltex.png','https://res.cloudinary.com/edwin3002/image/upload/v1656224270/portafolio/landing-2_qtlkwb.png','https://res.cloudinary.com/edwin3002/image/upload/v1656224269/portafolio/landing-3_cvnelr.png'],
+        github: 'https://github.com/Edwin3002/landing-page-Fylo',
+        figma: ['', ''],
+        technology: 'React.js, FontAwesome, Chakra ui.',
+        descrption: ''
+    },
+    {
+        id: 6,
+        name: 'Figuras',
+        url: 'https://figuritas.netlify.app/',
+        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656224276/portafolio/figuritas-1_b7fkpr.png','https://res.cloudinary.com/edwin3002/image/upload/v1656224277/portafolio/figuritas-2_aonuve.png','https://res.cloudinary.com/edwin3002/image/upload/v1656224265/portafolio/figuritas-3_jlglrt.png'],
+        github: 'https://github.com/Edwin3002/Figuras',
+        figma: ['', ''],
+        technology: 'React.js, SweetAlert2, Material ui, Heroku.',
+        descrption: ''
+    },
+    {
+        id: 7,
+        name: 'Cards',
+        url: 'https://card-ed.netlify.app/',
+        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656224266/portafolio/cards-1_bldu6e.png','https://res.cloudinary.com/edwin3002/image/upload/v1656224265/portafolio/cards-3_pq9d4p.png', 'https://res.cloudinary.com/edwin3002/image/upload/v1656224266/portafolio/cards-2_ogeuya.png'],
+        github: 'https://github.com/Edwin3002/Cards',
+        figma: ['', ''],
+        technology: 'React.js, Styled-components.',
+        descrption: ''
+    }
+    ,
+    {
+        id: 8,
+        name: 'Tiendita',
+        url: 'https://github.com/Edwin3002/Tiendita',
+        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656224277/portafolio/tiendita-1_j5bd5v.png','https://res.cloudinary.com/edwin3002/image/upload/v1656224270/portafolio/tiendita-2_idwftd.png','https://res.cloudinary.com/edwin3002/image/upload/v1656224271/portafolio/tiendita-3_f6rimt.png'],
+        github: 'https://github.com/Edwin3002/Cards',
+        figma: ['', ''],
+        technology: 'Javascript, BootStrap, SweetAlert2.',
+        descrption: ''
+    } ,
+    {
+        id: 9,
+        name: 'Api World',
+        url: 'https://edwin3002.github.io/reto-paises2/html/index.html',
+        img: ['https://res.cloudinary.com/edwin3002/image/upload/v1656224981/portafolio/world-1_eemsr5.png','https://res.cloudinary.com/edwin3002/image/upload/v1656224993/portafolio/world-2_r63g89.png','https://res.cloudinary.com/edwin3002/image/upload/v1656224997/portafolio/world-3_cgjp9a.png'],
+        github: 'https://github.com/Edwin3002/Edwin3002.github.io/tree/main/reto-paises2',
+        figma: ['', ''],
+        technology: 'Javascript, BootStrap, Api.',
+        descrption: ''
+    }
+    // ,
     // {
-    //     id: 5,
-    //     name: 'Portafolio',
-    //     url: 'https://edwin-dev.vercel.app/',
-    //     img: '',
-    //     github: 'https://github.com/Edwin3002/portafolio',
-    //     figma: ['https://www.figma.com/file/Vx51s3MdtYjORzN6ixQ4Kw/Portafolio?node-id=0%3A1', ''],
-    //     technology: 'React.js, Next.js, Tailwind, Figma.',
+    //     id:10,
+    //     name: 'Cards',
+    //     url: 'https://card-ed.netlify.app/',
+    //     img: ['','',''],
+    //     github: 'https://github.com/Edwin3002/Cards',
+    //     figma: ['', ''],
+    //     technology: 'React.js, Styled-components.',
     //     descrption: ''
     // }
 ]
