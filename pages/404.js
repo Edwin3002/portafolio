f1d45f272b2fd50b2f79efb917e89233f7e8c2c7
Edwin3002 <vargas11742@gmail.com>
2022-06-28 12:33:21 -0500

    responsive page 404

--- pages/404.js
+++ pages/404.js
@@ -13,10 +13,10 @@ const Error404 = () => {
       </div>
       <div className='flex flex-col w-full lg:w-1/2 h-1/2 lg:h-screen justify-evenly  bg-purple-800'>
 
-        <div className='house flex flex-col mx-auto w-1/4 lg:w-1/2'>
+        <div className='house flex flex-col mx-auto w-1/2  sm:w-1/4 lg:w-1/2'>
           <Home />
           <a className='mx-auto my-4 w-3/4' href="https://www.edwin-dev.tech/">
-        <button  className='w-full mx-auto text-center text-xl lg:text-2xl font-bold'>Go Home</button>
+        <button  className='w-full mx-auto text-center text-lg lg:text-2xl font-bold'>Go Home</button>
           </a>
         </div>
       </div>
